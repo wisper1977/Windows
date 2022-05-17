efd7a65ab705b7cd4350cdd48d898f6a4a6e614e
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2022-05-17 10:49:50 -0700

    Update RemoveDomain.ps1

--- RemoveDomain.ps1
+++ RemoveDomain.ps1
@@ -1,6 +1,10 @@
-<# Powershell Script to remove computer from Domain
+﻿<#
+    .SYNOPSIS
+    Removes computer from domain. Make sure to setup your domain,User name, and Password.
+    .NOTES
+    filename: RemoveDomain.ps1
     author: Chris Collins
-    Date: 5/6/2022
+    created: 5/6/2022
     #>
     
 $DomainName = "DOMAIN" # Add your domain name here
